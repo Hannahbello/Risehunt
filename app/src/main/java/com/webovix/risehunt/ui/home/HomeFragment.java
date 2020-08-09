37d55a736b85e192f1c990b0f1788aaad71c8ff7
Hannahbello <bellohannah28@gmail.com>
2020-08-09 03:22:58 +0000

    for the error

--- app/src/main/java/com/webovix/risehunt/ui/home/HomeFragment.java
+++ app/src/main/java/com/webovix/risehunt/ui/home/HomeFragment.java
@@ -1,13 +1,14 @@
 package com.webovix.risehunt.ui.home;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -18,9 +19,22 @@ public class HomeFragment extends Fragment {
 
     private View homefragment;
 
+    Toolbar toolbar;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homefragment = inflater.inflate(R.layout.fragment_home, container, false);
+        toolbar = homefragment.findViewById(R.id.home_Toolbar);
         return homefragment;
     }
+
+
 }
